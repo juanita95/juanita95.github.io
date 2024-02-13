1dfe72b39ddd6855bed5bf541bd5176eac09d38e
juanita3 <jaranzazu@juridigo.com>
2024-02-13 15:09:19 -0500

    fix: cambio name app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   spotify_api: 'https://api.spotify.com/v1',
-  redirect_uri: 'https://juanita95.github.io/my-music-app',
+  redirect_uri: 'https://juanita95.github.io/juanitafy',
   login_uri: 'https://accounts.spotify.com/api/token',
   client_id: '708f3214cf2b4fafaa2a7907fa2553c6',
   client_secret: 'b4f6c8e8b54b4d278acafbc46a00dc06',
